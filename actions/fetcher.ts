6312586249326b8c18a20145929d74b6e950d0ac
nguyen.nk <khanhnguyen.vlu@gmail.com>
2023-11-04 00:39:13 +0700

    #UI create room and display dishes

---
+++ actions/fetcher.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import prisma from "@libs/prismadb";
+
+interface RoomDataProps {
+  roomId: string;
+}
+
+interface MenuDataProps {
+  restaurantId: number;
+  deliveryId: number;
+}
+
+export async function getRoomData({ roomId }: RoomDataProps) {
+  const room = await prisma.room.findUnique({
+    where: {
+      id: roomId,
+    },
+  });
+  return room;
+}
+
+export async function getMenuData({ restaurantId, deliveryId }: MenuDataProps) {
+  const menu = await prisma.menu.findMany({
+    where: {
+      restaurantId,
+      deliveryId,
+    },
+  });
+  return menu;
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
   images: {
     domains: [
       "encrypted-tbn0.gstatic.com",
