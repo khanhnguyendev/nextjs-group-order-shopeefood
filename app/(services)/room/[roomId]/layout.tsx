2ef7d3fcc25881d2b3671e246b06b1afbf7e017a
nguyen.nk <khanhnguyen.vlu@gmail.com>
2023-11-03 15:39:27 +0700

    #API create room and save dishes to DB

---
+++ app/(services)/room/[roomId]/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MenuList from "@/app/components/room/MenuList";
+
+const RoomIdLayout = async ({ params }: { params: { roomId: string } }) => {
+  return (
+    <div className="flex justify-center flex-wrap gap-4">
+      <MenuList roomId={params.roomId} />
+    </div>
+  );
+};
+
+export default RoomIdLayout;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["encrypted-tbn0.gstatic.com"],
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "neo-brutalism-ui-library.vercel.app",
+      "images.foody.vn",
+    ],
   },
 };
 
