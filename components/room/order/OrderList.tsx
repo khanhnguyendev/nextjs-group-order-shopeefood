b17f5e91ee9963ac221d1f7f72e42b3713d90f23
nguyen.nk <khanhnguyen.vlu@gmail.com>
2023-11-16 11:29:26 +0700

    #UI modal update & delete order

--- components/room/order/OrderList.tsx
+++ components/room/order/OrderList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { FaRegEdit } from "react-icons/fa";
 import { MdOutlineDeleteForever } from "react-icons/md";
 import { toast } from "react-toastify";
@@ -8,6 +9,8 @@ import { getOrderByRoomId } from "@/actions/order";
 import { formatPrice } from "@/utils/pricingUtils";
 
 import OrderName from "@/components/room/order/OrderName";
+import UpdateModal from "./UpdateModal";
+import Modal from "@/components/Modal";
 
 type Props = {
   params: {
@@ -16,7 +19,11 @@ type Props = {
 };
 
 const OrderList = ({ params }: Props) => {
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [newQuantity, setNewQuantity] = useState(1);
+  const [newNote, setNewNote] = useState("");
   const [orders, setOrderData] = useState<Order[] | null>();
 
   const { roomId } = params;
@@ -28,12 +35,28 @@ const OrderList = ({ params }: Props) => {
     });
   }, [roomId]);
 
-  const editOrder = () => {
-    toast.warning("Chưa làm ^.^");
+  const openUpdateModal = () => {
+    setIsUpdateModalOpen(true);
   };
 
-  const deleteOrder = () => {
-    toast.warning("Chưa làm ^.^");
+  const closeUpdateModal = () => {
+    setIsUpdateModalOpen(false);
+  };
+
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const updateOrder = (orderId: string) => {
+    toast.warning(`Updating order ${orderId}`);
+  };
+
+  const deleteOrder = (orderId: string) => {
+    toast.error(`Deleting order ${orderId}`);
   };
 
   if (isLoading) {
@@ -147,51 +170,173 @@ const OrderList = ({ params }: Props) => {
               <tbody>
                 {orders &&
                   orders.map((order) => (
-                    <tr key={order.id}>
-                      <th>
-                        <label>
-                          <input type="checkbox" className="checkbox" />
-                        </label>
-                      </th>
-                      {/* Name */}
-                      <td>
-                        <OrderName params={{ order: order }} />
-                      </td>
-                      {/* Food */}
-                      <td>
-                        {order.name} x {order.quantity}
-                        <br />
-                        <span className="badge badge-ghost badge-sm">
-                          {order.price}
-                        </span>
-                        <br />
-                        {order.note && (
-                          <span className="badge badge-secondary badge-sm">
-                            {order.note}
-                          </span>
-                        )}
-                      </td>
-                      {/* Subtotal */}
-                      <td>
-                        <div className="badge badge-secondary badge-outline">
-                          {formatPrice(order.amount)}
+                    <>
+                      {/* Update Order Modal */}
+                      <Modal
+                        isOpen={isUpdateModalOpen}
+                        title="Update your order"
+                      >
+                        <div
+                          key={order.id}
+                          className="flex flex-wrap justify-center items-center gap-5"
+                        >
+                          {/* Food Title */}
+                          <input
+                            type="text"
+                            value={order.name}
+                            className="input input-bordered w-3/5"
+                            disabled
+                          />
+
+                          {/* Food Note */}
+                          <input
+                            type="text"
+                            value={order.note}
+                            className="input input-bordered w-3/5"
+                            onChange={(e) => setNewNote(e.target.value)}
+                          />
+
+                          {/* Food Quantity */}
+                          <div className="flex justify-center items-center w-full">
+                            <input
+                              type="number"
+                              min={1}
+                              max={10}
+                              className="input input-bordered w-1/5"
+                              value={order.quantity}
+                              onChange={(e) =>
+                                setNewQuantity(e.target.valueAsNumber)
+                              }
+                            />
+                          </div>
+
+                          {/* Button Actions */}
+                          <div className="flex flex-wrap w-full justify-center items-center">
+                            <button
+                              className="btn btn-ghost"
+                              onClick={() => {
+                                closeUpdateModal();
+                              }}
+                            >
+                              Close
+                            </button>
+                            <button
+                              className="btn btn-success ml-2 px-6 py-[5px]"
+                              onClick={() => {
+                                closeUpdateModal();
+                                updateOrder(order.id);
+                              }}
+                            >
+                              Confirm
+                            </button>
+                          </div>
                         </div>
-                      </td>
-                      <th>
-                        <button
-                          className="btn btn-outline btn-success btn-xs mr-1"
-                          onClick={editOrder}
+                      </Modal>
+                      {/* Delete Order Modal */}
+                      <Modal
+                        isOpen={isDeleteModalOpen}
+                        title="Delete your order"
+                      >
+                        <div
+                          key={order.id}
+                          className="flex flex-wrap justify-center items-center gap-5"
                         >
-                          <FaRegEdit />
-                        </button>
-                        <button
-                          className="btn btn-outline btn-error btn-xs"
-                          onClick={deleteOrder}
-                        >
-                          <MdOutlineDeleteForever />
-                        </button>
-                      </th>
-                    </tr>
+                          {/* Food Title */}
+                          <input
+                            type="text"
+                            value={order.name}
+                            className="input input-bordered w-3/5"
+                            disabled
+                          />
+
+                          {/* Food Note */}
+                          <input
+                            type="text"
+                            value={order.note}
+                            className="input input-bordered w-3/5"
+                            disabled
+                          />
+
+                          {/* Food Quantity */}
+                          <div className="flex justify-center items-center w-full">
+                            <input
+                              type="number"
+                              min={1}
+                              max={10}
+                              className="input input-bordered w-1/5"
+                              value={order.quantity}
+                              disabled
+                            />
+                          </div>
+
+                          {/* Button Actions */}
+                          <div className="flex flex-wrap w-full justify-center items-center">
+                            <button
+                              className="btn btn-ghost"
+                              onClick={() => {
+                                closeDeleteModal();
+                              }}
+                            >
+                              Close
+                            </button>
+                            <button
+                              className="btn btn-success ml-2 px-6 py-[5px]"
+                              onClick={() => {
+                                closeDeleteModal();
+                                deleteOrder(order.id);
+                              }}
+                            >
+                              Confirm
+                            </button>
+                          </div>
+                        </div>
+                      </Modal>
+                      <tr>
+                        <th>
+                          <label>
+                            <input type="checkbox" className="checkbox" />
+                          </label>
+                        </th>
+                        {/* Name */}
+                        <td>
+                          <OrderName params={{ order: order }} />
+                        </td>
+                        {/* Food */}
+                        <td>
+                          {order.name} x {order.quantity}
+                          <br />
+                          <span className="badge badge-ghost badge-sm">
+                            {order.price}
+                          </span>
+                          <br />
+                          {order.note && (
+                            <span className="badge badge-secondary badge-sm">
+                              {order.note}
+                            </span>
+                          )}
+                        </td>
+                        {/* Subtotal */}
+                        <td>
+                          <div className="badge badge-secondary badge-outline">
+                            {formatPrice(order.amount)}
+                          </div>
+                        </td>
+                        <th>
+                          <button
+                            className="btn btn-outline btn-success btn-xs mr-1"
+                            onClick={openUpdateModal}
+                          >
+                            <FaRegEdit />
+                          </button>
+                          <button
+                            className="btn btn-outline btn-error btn-xs"
+                            onClick={openDeleteModal}
+                          >
+                            <MdOutlineDeleteForever />
+                          </button>
+                        </th>
+                      </tr>
+                    </>
                   ))}
               </tbody>
 
